41456a3954f69d75d6e017b57b5c27dc87912b76
xhbl <newxhbl@hotmail.com>
2026-02-22 21:41:45 -0800

    Retrieve version number from package.json.

--- vnbook/src/constants/appInfo.ts
+++ vnbook/src/constants/appInfo.ts
@@ -1,13 +1,14 @@
 /**
  * Application information constants
  */
+import packageJson from '../../package.json'
 
 export const APP_NAME = '单词记录本'
 
 export const APP_COPYRIGHT = '著左权所有 (C) 2026 XHBL'
 
 export function getAppVersion(): string {
-  return import.meta.env.VITE_APP_VERSION || '2.1'
+  return packageJson.version || '2.1'
 }
 
 export function getAppAboutText(): string {
